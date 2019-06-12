ac55534cbc3cc240108fdaaf6847e04efbc053c3
qiushi123 <2501902696@qq.com>
2019-06-12 17:15:00 +0800

    多文件上传

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 - [java入门003～手把手教你开发自己的第一个java项目(基于springboot2.1.5)](https://www.jianshu.com/p/fe1ec90eb99e)
 - [java入门004～五分钟教你用java开发一个小程序后台服务器～看完你也会](https://www.jianshu.com/p/23bb2aac3986)
 - [java入门005～springboot实现单个文件上传（图片 文档 视频 音频都可以上传）](https://www.jianshu.com/p/4dfbdc28b7c0)
-
+- [java入门006～springboot实现多文件的上传（java多文件的上传）](https://www.jianshu.com/p/52175a3216c0)
 # 微信交流
 2501902696（备注java）
 
--- src/main/java/com/shitou/springbootdemos/uploadfile/UploadController.java
+++ src/main/java/com/shitou/springbootdemos/uploadfile/UploadController.java
@@ -11,12 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by qcl on 2019-06-11
- * 微信：2501902696
  * desc: 文件上传
  */
 @RestController
 public class UploadController {
 
+    //单个文件的上传
     @PostMapping("/upload")
     public String upload(MultipartFile uploadFile, HttpServletRequest request) {
         /*
@@ -43,6 +43,45 @@ public class UploadController {
                     + "/uploadFile/" + filename;
             //3，返回可供访问的网络路径
             return filePath;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
+
+    //多个文件的上传
+    @PostMapping("/uploads")
+    public String uploads(MultipartFile[] uploadFiles, HttpServletRequest request) {
+        //1，对文件数组做判空操作
+        if (uploadFiles == null || uploadFiles.length < 1) {
+            return "文件不能为空";
+        }
+        //2，定义文件的存储路径,
+        String realPath = request.getSession().getServletContext().getRealPath("/uploadFile/");
+        File dir = new File(realPath);
+        if (!dir.isDirectory()) {//文件目录不存在，就创建一个
+            dir.mkdirs();
+        }
+
+        try {
+            String filePathS = "";
+            //3，遍历文件数组，一个个上传
+            for (int i = 0; i < uploadFiles.length; i++) {
+                MultipartFile uploadFile = uploadFiles[i];
+                String filename = uploadFile.getOriginalFilename();
+                //服务端保存的文件对象
+                File fileServer = new File(dir, filename);
+                System.out.println("file文件真实路径:" + fileServer.getAbsolutePath());
+                //2，实现上传
+                uploadFile.transferTo(fileServer);
+                String filePath = request.getScheme() + "://" +
+                        request.getServerName() + ":"
+                        + request.getServerPort()
+                        + "/uploadFile/" + filename;
+                filePathS = filePathS + "\n" + filePath;
+            }
+            //4，返回可供访问的网络路径
+            return filePathS;
         } catch (IOException e) {
             e.printStackTrace();
         }
